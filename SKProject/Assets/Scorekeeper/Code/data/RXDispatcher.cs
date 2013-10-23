966f809f963eebcc73b2155103e8157a4d5a1385
Matt Rix <matt@matt-rix.com>
2013-10-23 18:59:05 -0400

    Creating a simple event dispatcher
    This is basically a very simplified version of the Flash event
    dispatcher... I'm not really sure why I made it but it seemed worthwhile
    at the time :)

---
+++ SKProject/Assets/Scorekeeper/Code/data/RXDispatcher.cs
@@ -0,0 +1,121 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class RXDispatcher
+{
+	public delegate void NoArgumentDelegate();
+
+	private List<RXListener> _listeners;
+
+	public RXDispatcher()
+	{
+
+	}
+
+	public void Dispatch(int eventType)
+	{
+		for(int n = 0; n<_listeners.Count;n++)
+		{
+			RXListener listener = _listeners[n];
+
+			if(listener.eventType == eventType)
+			{
+				if(_listeners[n].isWeak)
+				{
+					object target = _listeners[n].weakRef.Target;
+					if(target != null)
+					{
+						(target as NoArgumentDelegate).Invoke();
+					}
+					else //remove because it's null
+					{
+						_listeners.RemoveAt(n);
+						n--;
+					}
+				}
+				else
+				{
+					_listeners[n].strongDele.Invoke();
+				}
+			}
+		}
+	}
+
+	public void AddStrongListener(int eventType, NoArgumentDelegate dele)
+	{
+		AddListener(eventType,dele,false);
+	}
+
+	public void AddWeakListener(int eventType, NoArgumentDelegate dele)
+	{
+		AddListener(eventType,dele,true);
+	}
+
+	public void AddListener(int eventType, NoArgumentDelegate dele, bool isWeak)
+	{
+		if(_listeners == null) _listeners = new List<RXListener>();
+
+		RXListener listener = new RXListener();
+
+		listener.eventType = eventType;
+
+		if(isWeak)
+		{
+			listener.isWeak = true;
+			listener.weakRef = new WeakReference(dele);
+		}
+		else 
+		{
+			listener.isWeak = false;
+			listener.strongDele = dele;
+		}
+
+		_listeners.Add(listener);
+	}
+
+	public void RemoveListener(int eventType, NoArgumentDelegate dele)
+	{
+		if(_listeners != null) 
+		{
+			for(int n = _listeners.Count-1; n>=0;n--)
+			{
+				if(_listeners[n].eventType == eventType)
+				{
+					if(_listeners[n].isWeak)
+					{
+						NoArgumentDelegate target = _listeners[n].weakRef.Target as NoArgumentDelegate;
+						if(target != null && target == dele)
+						{
+							_listeners.RemoveAt(n);
+						}
+
+					}
+					else if(_listeners[n].strongDele == dele)
+					{
+						_listeners.RemoveAt(n);
+					}
+				}
+			}
+
+		}
+	}
+
+	public void RemoveAllListeners()
+	{
+		if(_listeners != null)
+		{
+			_listeners.Clear();
+		}
+	}
+
+	private class RXListener
+	{
+		public bool isWeak;
+		public WeakReference weakRef;
+		public int eventType;
+		public NoArgumentDelegate strongDele;
+	}
+
+}
+
---
+++ SKProject/Assets/Scorekeeper/Code/display/Box.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class Box : FContainer
+{
+	public List<FSprite> foregroundSprites = new List<FSprite>();
+
+	public Box ()
+	{
+
+	}
+}
+
