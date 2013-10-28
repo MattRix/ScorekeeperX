fecff6696dbdbc0884d2d64d819e82ff0da98655
Matt Rix <matt@matt-rix.com>
2013-10-27 23:29:09 -0400

    CLeaning up the ease a bit more

--- SKProject/Assets/Futile/Rix/RXUtils.cs
+++ SKProject/Assets/Futile/Rix/RXUtils.cs
@@ -485,25 +485,25 @@ public static class RXEase
 {
 	public delegate float RXEaseDelegate(float t);
 
-	public static RXEaseDelegate Linear = (t) => { return t; };
-	public static RXEaseDelegate QuadIn = (t) => { return t * t; };
-	public static RXEaseDelegate QuadOut = (t) => { return 1 - QuadIn(1 - t); };
-	public static RXEaseDelegate QuadInOut = (t) => { return (t <= 0.5f) ? QuadIn(t * 2) / 2 : QuadOut(t * 2 - 1) / 2 + 0.5f; };
-	public static RXEaseDelegate CubeIn = (t) => { return t * t * t; };
-	public static RXEaseDelegate CubeOut = (t) => { return 1 - CubeIn(1 - t); };
-	public static RXEaseDelegate CubeInOut = (t) => { return (t <= 0.5f) ? CubeIn(t * 2) / 2 : CubeOut(t * 2 - 1) / 2 + 0.5f; };
-	public static RXEaseDelegate BackIn = (t) => { return t * t * (2.70158f * t - 1.70158f); };
-	public static RXEaseDelegate BackOut = (t) => { return 1 - BackIn(1 - t); };
-	public static RXEaseDelegate BackInOut = (t) => { return (t <= 0.5f) ? BackIn(t * 2) / 2 : BackOut(t * 2 - 1) / 2 + 0.5f; };
-	public static RXEaseDelegate ExpoIn = (t) => { return (float)Mathf.Pow(2, 10 * (t - 1)); };
-	public static RXEaseDelegate ExpoOut = (t) => { return 1 - ExpoIn(t); };
-	public static RXEaseDelegate ExpoInOut = (t) => { return t < .5f ? ExpoIn(t * 2) / 2 : ExpoOut(t * 2) / 2; };
-	public static RXEaseDelegate SineIn = (t) => { return -Mathf.Cos(Mathf.PI / 2 * t) + 1; };
-	public static RXEaseDelegate SineOut = (t) => { return Mathf.Sin(Mathf.PI / 2 * t); };
-	public static RXEaseDelegate SineInOut = (t) => { return -Mathf.Cos(Mathf.PI * t) / 2f + .5f; };
-	public static RXEaseDelegate ElasticIn = (t) => { return 1 - ElasticOut(1 - t); };
-	public static RXEaseDelegate ElasticOut = (t) => { return Mathf.Pow(2, -10 * t) * Mathf.Sin((t - 0.075f) * (2 * Mathf.PI) / 0.3f) + 1; };
-	public static RXEaseDelegate ElasticInOut = (t) => { return (t <= 0.5f) ? ElasticIn(t * 2) / 2 : ElasticOut(t * 2 - 1) / 2 + 0.5f; };
+	public static RXEaseDelegate Linear = (t) => 		{ return t; };
+	public static RXEaseDelegate QuadIn = (t) => 		{ return t * t; };
+	public static RXEaseDelegate QuadOut = (t) => 		{ return 1 - QuadIn(1 - t); };
+	public static RXEaseDelegate QuadInOut = (t) => 	{ return (t <= 0.5f) ? QuadIn(t * 2) / 2 : QuadOut(t * 2 - 1) / 2 + 0.5f; };
+	public static RXEaseDelegate CubeIn = (t) => 		{ return t * t * t; };
+	public static RXEaseDelegate CubeOut = (t) => 		{ return 1 - CubeIn(1 - t); };
+	public static RXEaseDelegate CubeInOut = (t) => 	{ return (t <= 0.5f) ? CubeIn(t * 2) / 2 : CubeOut(t * 2 - 1) / 2 + 0.5f; };
+	public static RXEaseDelegate BackIn = (t) => 		{ return t * t * (2.70158f * t - 1.70158f); };
+	public static RXEaseDelegate BackOut = (t) => 		{ return 1 - BackIn(1 - t); };
+	public static RXEaseDelegate BackInOut = (t) => 	{ return (t <= 0.5f) ? BackIn(t * 2) / 2 : BackOut(t * 2 - 1) / 2 + 0.5f; };
+	public static RXEaseDelegate ExpoIn = (t) => 		{ return Mathf.Pow(2, 10 * (t - 1)); };
+	public static RXEaseDelegate ExpoOut = (t) => 		{ return 1 - Mathf.Pow(2, 10 * (t - 1)); };
+	public static RXEaseDelegate ExpoInOut = (t) => 	{ return t < .5f ? ExpoIn(t * 2) / 2 : ExpoOut(t * 2) / 2; };
+	public static RXEaseDelegate SineIn = (t) => 		{ return -Mathf.Cos(Mathf.PI / 2 * t) + 1; };
+	public static RXEaseDelegate SineOut = (t) => 		{ return Mathf.Sin(Mathf.PI / 2 * t); };
+	public static RXEaseDelegate SineInOut = (t) => 	{ return -Mathf.Cos(Mathf.PI * t) / 2f + .5f; };
+	public static RXEaseDelegate ElasticIn = (t) => 	{ return 1 - ElasticOut(1 - t); };
+	public static RXEaseDelegate ElasticOut = (t) => 	{ return Mathf.Pow(2, -10 * t) * Mathf.Sin((t - 0.075f) * (2 * Mathf.PI) / 0.3f) + 1; };
+	public static RXEaseDelegate ElasticInOut = (t) => 	{ return (t <= 0.5f) ? ElasticIn(t * 2) / 2 : ElasticOut(t * 2 - 1) / 2 + 0.5f; };
 }
 
 
